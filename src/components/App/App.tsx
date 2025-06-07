c19e77be2997d586415b12f1585a8611e8300ba1
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-06-07 03:32:59 +0300

    done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Movie } from "../../types/movie";
 import { getMovies } from "../../services/movieService";
 import toast, { Toaster } from "react-hot-toast";
@@ -8,28 +8,17 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 
 export default function App() {
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+    const [query, setQuery] = useState("");
+    const [page, setPage] = useState(1);
     const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-    const handleSearchBar = async (query: string) => {
-        setIsLoading(true);
-        setIsError(false);
-        setMovies([]);
-        try {
-            const results = await getMovies(query);
-            if (results.length === 0) {
-                toast.error('No movies found for your request.');
-            }
-            setMovies(results);
-        } catch {
-             setIsError(true);
-        } finally {
-             setIsLoading(false);
-        }
+    const handleSearchBar = (newQuery: string) => {
+        setQuery(newQuery);
+        setPage(1);
     };
 
     const handleSelectMovie = (movie: Movie) => {
@@ -40,12 +29,44 @@ export default function App() {
         setSelectedMovie(null);
     };
 
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["movies", query, page],
+        queryFn: () => getMovies(query, page),
+        placeholderData: keepPreviousData,
+    });
+
+    const movies = data?.results ?? [];
+    const totalPages = data?.total_pages ?? 0;
+
+    useEffect(() => {
+        if (!isLoading && !isError && query && movies.length === 0) {
+            toast.error('No movies found for your request.');
+        }
+    }, [isLoading, isError, query, movies.length]);
+
     return <div className={css.app}>
         <SearchBar onSubmit={handleSearchBar}/>
         <Toaster />
         {isLoading && <Loader />}
         {isError && <ErrorMessage />}
-        {!isLoading && !isError && movies.length > 0 && (<MovieGrid movies={movies} onSelect={handleSelectMovie}/>)}
+        {!isLoading && !isError && movies.length > 0 && (
+            <>
+            {totalPages > 1 && (
+            <ReactPaginate
+              pageCount={totalPages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={css.pagination}
+              activeClassName={css.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+            <MovieGrid movies={movies} onSelect={handleSelectMovie}/>
+            </>
+            )}
         {selectedMovie && (<MovieModal movie={selectedMovie} onClose={handleCloseModal} />)}
     </div>
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -5,17 +5,23 @@ const BASE_URL = "https://api.themoviedb.org/3/search/movie";
 
 interface MovieProps {
   results: Movie[];
+  page: number;
+  total_pages: number;
 }
 
-export const getMovies = async (query: string): Promise<Movie[]> => {
+export const getMovies = async (
+  query: string,
+  page: number = 1
+): Promise<MovieProps> => {
   const { data } = await axios.get<MovieProps>(BASE_URL, {
     params: {
       query,
+      page,
       adult: false,
     },
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
   });
-  return data.results;
+  return data;
 };
