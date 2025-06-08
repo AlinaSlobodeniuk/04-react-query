6eb9399e64676ada22e6c72f9cc87f51c456a56a
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-06-08 12:12:32 +0300

    fix2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -32,7 +32,7 @@ export default function App() {
     const { data, isLoading, isError } = useQuery({
         queryKey: ["movies", query, page],
         queryFn: () => getMovies(query, page),
-        enabled: true,
+        enabled: !!query,
         placeholderData: keepPreviousData,
     });
 
